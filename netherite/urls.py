029eabe75504dbf3bdadde1c0a6ae5879a32f1bf
panchicore <thepanchi@gmail.com>
2022-11-12 12:26:45 -0500

    add vehicle endpoints

--- netherite/urls.py
+++ netherite/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 from accounts.views import CompanyViewSet, AccountViewSet
 from clients.views import ClientViewSet
 from drivers.views import DriverViewSet, DriverDocumentViewSet
+from vehicles.views import VehicleViewSet
 
 router = routers.DefaultRouter()
 router.register(r'accounts/companies', CompanyViewSet)
@@ -12,11 +13,11 @@ router.register(r'accounts', AccountViewSet)
 router.register(r'clients', ClientViewSet)
 router.register(r'drivers/docs', DriverDocumentViewSet)
 router.register(r'drivers', DriverViewSet)
+router.register(r'vehicles', VehicleViewSet)
 
 urlpatterns = [
     path('api/1/', include(router.urls)),
     path('nimda/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/auth/', include('dj_rest_auth.urls')),
     path('activity/', include('actstream.urls'))
 ]
